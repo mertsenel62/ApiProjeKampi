0f6de07c036df2c385efcd417f53e130d64ddaa4
Mert <mertsenel6223@gmail.com>
2026-07-07 16:28:57 +0300

    Contact API işlemleri tamamlandı

---
+++ ApiProjeKampi.WebApi/Controllers/ContactsController.cs
@@ -0,0 +1,70 @@
+﻿using ApiProjeKampi.WebApi.Context;
+using ApiProjeKampi.WebApi.Dtos.ContactDtos;
+using ApiProjeKampi.WebApi.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjeKampi.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly ApiContext _context;
+        public ContactsController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult ContactList()
+        {
+            var values = _context.Conctacts.ToList();
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult CreateContact(CreateContactDto createContactDto)
+        {
+            Contact contact = new Contact();
+            contact.Email = createContactDto.Email;
+            contact.Address = createContactDto.Address;
+            contact.Phone = createContactDto.Phone;
+            contact.MapLocation = createContactDto.MapLocation;
+            contact.OpenHours = createContactDto.OpenHours;
+            _context.Conctacts.Add(contact);
+            _context.SaveChanges();
+            return Ok("Ekleme işlemi başarılı");
+        }
+        [HttpDelete]
+        public IActionResult DeleteContact(int id)
+        {
+            var value = _context.Conctacts.Find(id);
+            _context.Conctacts.Remove(value);
+            _context.SaveChanges();
+            return Ok("Silme işlemi başarılı");
+        }
+
+        [HttpGet("GetContact")]
+        public IActionResult GetContact(int id)
+        {
+            var value = _context.Conctacts.Find(id);
+            return Ok(value);
+        }
+        [HttpPut]
+        public IActionResult UpdateContact(UpdateContactDto updateContactDto)
+        {
+            Contact contact = new Contact();
+            contact.Email = updateContactDto.Email;
+            contact.Address = updateContactDto.Address;
+            contact.Phone = updateContactDto.Phone;
+            contact.ContactId = updateContactDto.ContactId;
+            contact.MapLocation = updateContactDto.MapLocation;
+            contact.OpenHours = updateContactDto.OpenHours;
+            _context.Conctacts.Update(contact);
+            _context.SaveChanges();
+            return Ok("Güncelleme işlemi başarılı");
+        }
+
+    }
+}
