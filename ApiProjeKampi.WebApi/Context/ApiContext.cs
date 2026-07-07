11f7cd3f26b419c5555a90a31485d531cab557f9
Mert <mertsenel6223@gmail.com>
2026-07-07 14:31:34 +0300

    Add project files.

---
+++ ApiProjeKampi.WebApi/Context/ApiContext.cs
@@ -0,0 +1,27 @@
+﻿using ApiProjeKampi.WebApi.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiProjeKampi.WebApi.Context
+{
+    public class ApiContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=BABAJOE\\SQLEXPRESS;initial catalog=ApiYummyDb;" +
+                "integrated security=true;");
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Chef> Chefs { get; set; }
+        public DbSet<Contact> Conctacts { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Rezervation> Rezerations { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+
+    }
+}
