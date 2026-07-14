29ff713d696d013351b52abdda2a043a60d0d217
Mert <mertsenel6223@gmail.com>
2026-07-14 12:10:25 +0300

    refactor chefs and footer into ViewComponents, add chef data

---
+++ ApiProjeKampi.WebUl/ViewComponents/_ChefDefaultComponentPartial.cs
@@ -0,0 +1,30 @@
+﻿using ApiProjeKampi.WebUl.Dtos.ChefDtos;
+using ApiProjeKampi.WebUl.Dtos.EventDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ApiProjeKampi.WebUl.ViewComponents
+{
+    public class _ChefDefaultComponentPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _ChefDefaultComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7003/api/Chefs");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultChefDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
---
+++ ApiProjeKampi.WebUl/ViewComponents/_FooterDefaultComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjeKampi.WebUl.ViewComponents
+{
+    public class _FooterDefaultComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
