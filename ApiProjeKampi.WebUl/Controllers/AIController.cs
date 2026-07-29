31929154dc94559d15051a3325fb5a36533b0c83
Mert <mertsenel6223@gmail.com>
2026-07-29 16:41:10 +0300

    OpenAI API entegrasyonu eklendi

---
+++ ApiProjeKampi.WebUl/Controllers/AIController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+namespace ApiProjeKampi.WebUl.Controllers
+{
+    public class AIController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public AIController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public IActionResult CreateRecipeWithOpenAI()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRecipeWithOpenAI(string prompt)
+        {
+            var apikey = _configuration["OpenAI:ApiKey"];
+
+            using var client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apikey);
+
+            var requestData = new
+            {
+                model = "gpt-3.5-turbo",
+                messages = new[]
+                {
+                    new
+                    {
+                        role="system",
+                        content="Sen bir restoran için yemek önerilerini yapan bir yapay zeka aracısın. Amacımız kullanıcı tarafından girilen malzemelere göre yemek tarifi önerisinde bulunmak."
+                    },
+                    new
+                    {
+                        role="user",
+                        content=prompt
+                    }
+                },
+                temperature = 0.5
+            };
+            var response = await client.PostAsJsonAsync("https://api.openai.com/v1/chat/completions", requestData);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadFromJsonAsync<OpenAIResponse>();
+                var content = result.choices[0].message.content;
+                ViewBag.recipe = content;
+            }
+            else
+            {
+                var error = await response.Content.ReadAsStringAsync();
+
+                ViewBag.recipe =
+                    $"Hata: {response.StatusCode}\n\n{error}";
+            }
+
+            return View();
+
+        }
+
+        public class OpenAIResponse
+        {
+            public List<Choice> choices { get; set; }
+        }
+
+        public class Choice
+        {
+            public Message message { get; set; }
+        }
+        public class Message
+        {
+            public string role { get; set; }
+            public string content { get; set; }
+        }
+    }
+}
