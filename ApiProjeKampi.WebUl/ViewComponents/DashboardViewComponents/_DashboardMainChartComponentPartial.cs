088588f5fd5b966355a75ad5d92abf4e58ccc3f5
Mert <mertsenel6223@gmail.com>
2026-08-05 16:16:41 +0300

    Chart grafiği eklendi

---
+++ ApiProjeKampi.WebUl/ViewComponents/DashboardViewComponents/_DashboardMainChartComponentPartial.cs
@@ -0,0 +1,30 @@
+﻿using ApiProjeKampi.WebUl.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ApiProjeKampi.WebUl.ViewComponents.DashboardViewComponents
+{
+    public class _DashboardMainChartComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var vm = new RevenueChartViewModel
+            {
+                Labels = new List<string> { "Jan", "Feb", "Mar", "Apr", "May", "Jun" },
+                Income = new List<int> { 5, 15, 14, 36, 32, 32 },
+                Expense = new List<int> { 7, 11, 30, 18, 25, 13 },
+                WeeklyEarnings = 675,
+                MonthlyEarnings = 1587,
+                YearlyEarnings = 45965,
+                TotalCustomers = 8257,
+                TotalIncome = 9857,
+                ProjectCompleted = 28,
+                TotalExpense = 6287,
+                NewCustomers = 684
+            };
+
+            return View(vm);
+        }
+    }
+}
