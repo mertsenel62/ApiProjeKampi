4b386b3de291ee84465307b352568c92e279c641
Mert <mertsenel6223@gmail.com>
2026-08-05 14:07:19 +0300

    Admin paneline Dahboard eklendi

---
+++ ApiProjeKampi.WebUl/Controllers/DashBoardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjeKampi.WebUl.Controllers
+{
+    public class DashBoardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
