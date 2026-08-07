058fe140e37a68f66a941af335f162f2533a7bc8
Mert <mertsenel6223@gmail.com>
2026-08-07 19:25:11 +0300

    Çoğu tablo için veri eklendi

---
+++ ApiProjeKampi.WebUl/ViewComponents/DashboardViewComponents/_DashboardEmployeeTaskComponentPartial.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjeKampi.WebUl.ViewComponents.DashboardViewComponents
+{
+    public class _DashboardEmployeeTaskComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DashboardEmployeeTaskComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }   
+    }
+}
